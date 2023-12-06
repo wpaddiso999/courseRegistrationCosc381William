3357c280d3311bb61cb6d32fc143f5ba2d69f1d1
William Paddison <138761963+wpaddiso999@users.noreply.github.com>
2023-12-06 15:50:06 -0500

    Update test_main.py

--- .github/workflows/test_main.py
+++ .github/workflows/test_main.py
@@ -1,24 +1,10 @@
+# tests/test_main.py
 from fastapi.testclient import TestClient
 from main import app
-from test_course import courseA
-from course import courses
 
 client = TestClient(app)
 
-def test_get_courses(courseA):
-    courses.append(courseA)
-
-    response = client.get("/courses/COSC")
+def test_register_course():
+    response = client.post("/register_course", json={"student_eid": "123", "course_prefix": "CSE", "course_number": "101"})
     assert response.status_code == 200
-    assert response.json() == [
-        {
-            "_prefix":"COSC",
-            "_course_number":"111",
-            "_cap":30,
-            "_instructor":"John Doe",
-            "_name":"Programming I",
-            "_place":"PH 503",
-            "_meeting_times":"TH 9:00"
-        }]
-
-    courses.pop()
+    assert response.json() == {"registered_courses": ["CSE-101"]}
