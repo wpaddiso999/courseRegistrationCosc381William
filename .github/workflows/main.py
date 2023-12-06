1aa15a3fdd06626b192381726301866801f4d300
William Paddison <138761963+wpaddiso999@users.noreply.github.com>
2023-12-06 15:47:58 -0500

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,15 +1,11 @@
+# main.py
 from fastapi import FastAPI
-from course import courses
+from student import Student
 
 app = FastAPI()
 
-
-@app.get("/courses/{prefix}")
-def get_courses(prefix: str):
-    # return all the courses under the prefix
-    results = []
-    for course in courses:
-        if course.is_prefix(prefix):
-            results.append(course)
-    
-    return results
+@app.post("/register_course")
+def register_course(student_eid: str, course_prefix: str, course_number: str):
+    student = Student(eid=student_eid)
+    result = student.register_course(course_prefix, course_number)
+    return {"registered_courses": result}
