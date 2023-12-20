b09d306205b188915fc19912afb16d8fb1cc5ed6
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2023-12-20 14:42:34 -0500

    Notes on this assignment

--- .github/workflows/test_student.py
+++ .github/workflows/test_student.py
@@ -2,6 +2,12 @@
 import pytest
 from student import Student
 
+## Move all the files out of the .github/workflows (except for the blank.yml)
+## Follow the GitHub flow (creating an issue, create a branch, commits, and do pull request)
+## You need to add the following items in "test_student.py":
+##   * fixture to create a Student in this file, and use the fixture in the test
+##   * mocker to mock the method "is_valid_course" in a new test
+
 def test_register_course():
     student = Student(eid="123")
     result = student.register_course("CSE", "101")
